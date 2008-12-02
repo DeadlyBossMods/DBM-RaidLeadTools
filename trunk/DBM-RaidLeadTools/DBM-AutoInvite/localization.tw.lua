86c697ebe4efee178b1d29365f270ad8c8aa0ae3
toni <toni@61060b52-c38c-4791-a31c-c37ca13f87f3>
2008-12-02 13:34:19 +0000

    zhCN & zhTW update/add

--- trunk/DBM-RaidLeadTools/DBM-AutoInvite/localization.tw.lua
+++ trunk/DBM-RaidLeadTools/DBM-AutoInvite/localization.tw.lua
@@ -1,11 +1,9 @@
-﻿if GetLocale() ~= "zhTW" then return end
-
-DBM_AutoInvite_Translations = {}
-
+﻿-- readjust by yleaf(yaroot@gmail.com)/Juha
+if GetLocale() ~= "zhTW" then return end
 local L = DBM_AutoInvite_Translations
 
-L.TabCategory_AutoInvite 	= "自動邀請工具"
-L.AreaGeneral 			= "基本邀請選項"
+L.TabCategory_AutoInvite 	= "邀請助手"
+L.AreaGeneral 			= "一般設定"
 L.AllowGuildMates 		= "允許來自公會成員的自動邀請"
 L.AllowFriends 			= "允許來自好友的自動邀請"
 L.AllowOthers 			= "允許來自任何人的自動邀請"
@@ -15,10 +13,10 @@ L.InviteFailed 			= "不能邀請玩家 %s"
 L.ConvertRaid 			= "轉換隊伍為團隊"
 L.WhisperMsg_RaidIsFull 	= "對不起，我不能邀請你。團隊已滿了。"
 L.WhisperMsg_NotLeader 		= "對不起，我不能邀請你。我不是組長。"
-L.WarnMsg_NoRaid		= "使用AoE-邀請前請先建立一個團隊"
-L.WarnMsg_NotLead		= "對不起，你必須為RL或提升才能使用此命令"
-L.WarnMsg_InviteIncoming	= "<DBM> AoE-邀請即將來臨! 請現在離開你的隊伍."
-L.Button_AOE_Invite		= "AoE 公會邀請"
+L.WarnMsg_NoRaid		= "使用集體邀請前請先建立一個團隊"
+L.WarnMsg_NotLead		= "對不起，你必須為團長或助理才能使用此命令"
+L.WarnMsg_InviteIncoming	= "<DBM> 收到集體邀請! 請現在離開你的隊伍."
+L.Button_AOE_Invite		= "集體邀請公會成員"
 L.AOEbyGuildRank		= "邀請所有等於或大於所選階級的玩家"
 
 -- RaidInvite Options
--- trunk/DBM-RaidLeadTools/DBM-BidBot/localization.tw.lua
+++ trunk/DBM-RaidLeadTools/DBM-BidBot/localization.tw.lua
@@ -1,14 +1,12 @@
-﻿if GetLocale() ~= "zhTW" then return end
-
-DBM_BidBot_Translations = {}
-
+-- readjust by yleaf(yaroot@gmail.com)/Juha
+if GetLocale() ~= "zhTW" then return end
 local L = DBM_BidBot_Translations
 
 L.Prefix = "[BidBot]: "
 
-L.Whisper_Queue 		= "另一個拍賣正在進行。您的裝備已排隊。"
+L.Whisper_Queue 		= "另一個拍賣正在進行。您的裝備已被列入到排隊中。"
 L.Whisper_Bid_OK 		= "您的出價，共%dDKP已被接受。"
-L.Whisper_InUse 		= "<remove me>"
+L.Whisper_InUse 		= "<移除我>"
 L.Message_StartBidding		= "現在競投 %s，請密[%s]出價! 最低出價: %d"
 L.Message_DoBidding		= "%s的剩餘時間: %d秒"
 L.Message_ItemGoesTo		= "%s 羸得 %s，使用 %d DKP!"
@@ -16,19 +14,19 @@ L.Message_NoBidMade		= "沒有人競投 %s."
 L.Message_Biddings		= "%d. %s 出價 %d DKP."
 L.Message_BiddingsVisible	= "%d 玩家競投這個裝備."
 L.Message_BidPubMessage		= "新出價: %s 出價 %d DKP"
-L.Disenchant			= "放棄"
+L.Disenchant			= "分解"
 
-L.PopUpAcceptDKP		= "對%s儲存出價. 如想放棄請輸入0 DKP."
+L.PopUpAcceptDKP		= "對%s儲存出價. 分解請輸入0 DKP."
 
 
 -- GUI
-L.TabCategory_BidBot	 	= "出價助手 (DKP)"
+L.TabCategory_BidBot	 	= "競拍助手 (DKP)"
 L.TabCategory_History	 	= "裝備記錄"
-L.AreaGeneral 			= "基本出價助手選項"
+L.AreaGeneral 			= "一般設定"
 L.AreaItemHistory		= "裝備記錄"
-L.Enable			= "開啟出價助手 (!bid [item])"
+L.Enable			= "開啟競拍助手 (!bid [item])"
 L.ChatChannel			= "使用下列頻道輸出"
-L.Local				= "只使用說話"
+L.Local				= "只在本地顯示"
 L.Guild				= "使用公會頻道"
 L.Raid				= "使用團隊頻道"
 L.Party				= "使用隊伍頻道"
@@ -42,8 +40,7 @@ L.PayWhatYouBid			= "支付的競標價格 (否則第二次出價+1)"
 L.NoHistoryAvailable		= "沒有有效記錄"
 L.DateFormat			= "%c"
 
-L.Button_ShowClients		= "顯示客戶"
-L.Button_ResetClient		= "重啟出價助手"
+L.Button_ShowClients		= "顯示插件版本"
+L.Button_ResetClient		= "重設競拍助手"
 L.Local_NoRaid			= "仕你必需在一個團隊才能使用這個功能"
 L.Local_Version			= "%s: %s"	-- Lacrosa: r123	(no translation required ^^)
-
