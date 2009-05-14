cccd7afcecb6caef951a309842ee0bf2dd7ca625
bluenyx <bluenyx@61060b52-c38c-4791-a31c-c37ca13f87f3>
2009-05-14 11:28:55 +0000

    koKR update

--- trunk/DBM-RaidLeadTools/DBM-RaidLeadTools.lua
+++ trunk/DBM-RaidLeadTools/DBM-RaidLeadTools.lua
@@ -12,6 +12,7 @@
 --    * deDE: Nitram/Tandanu             http://www.deadlybossmods.com
 --    * zhCN: yleaf(yaroot@gmail.com)
 --    * zhTW: yleaf(yaroot@gmail.com)/Juha
+--    * koKR: BlueNyx(bluenyx@gmail.com)
 --    * (add your names here!)
 --
 -- 
