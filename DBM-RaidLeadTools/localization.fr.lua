8ef6541816bd7847889226515d06ca3c7167635d
sasmira <sasmira@61060b52-c38c-4791-a31c-c37ca13f87f3>
2010-12-28 00:32:24 +0000

    frFR minor fix

--- DBM-RaidLeadTools/localization.fr.lua
+++ DBM-RaidLeadTools/localization.fr.lua
@@ -3,9 +3,9 @@ if GetLocale() ~= "frFR" then return end
 local L = DBM_Raidlead_Translation
 
 -- Initial translation by Sasmira
--- Last update: 12/27/2010 (by Sasmira)
+-- Last update: 12/28/2010 (by Sasmira)
 
-L.Area_Raidleadtool			= "Options supplémentaires: Raidlead"
+L.Area_Raidleadtool			= "Options supplémentaires: Raid Lead"
 L.ShowWarningForLootMaster		= "Afficher une alerter lorsque le combat commence si le Maitre du Butin n'est pas activé"
 
 L.Warning_NoLootMaster		= "Maitre des loots is désactivé - Merci de l'activer maintenant!"
